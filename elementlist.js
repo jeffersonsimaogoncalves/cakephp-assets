10318c3a631e09810eaea8dc1cd761c720d3b051
mirko-pagliai <mirko.pagliai@gmail.com>
2019-01-23 13:30:59 +0100

    added api

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","Assets\\Http\\Exception\\AssetNotFoundException"],["c","Assets\\Plugin"],["c","Assets\\Routing\\Middleware\\AssetMiddleware"],["c","Assets\\TestSuite\\TestCase"],["c","Assets\\Utility\\AssetsCreator"],["c","Assets\\View\\Helper\\AssetHelper"]];
